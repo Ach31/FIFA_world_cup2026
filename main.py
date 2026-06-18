dade19c00591ddddef1f07ddb92d6a0ec1aec64b
Ach31 <achillebergeret31@gmail.com>
2026-06-18 18:31:42 +0200

    analyse clubs player

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import kagglehub
 import os
 import pandas as pd
@@ -12,12 +13,23 @@ print("Fichiers disponibles :", files)
 
 # 3. Charge le fichier CSV dans un DataFrame Pandas
 csv_file = os.path.join(path, files[0])  # Prend le premier fichier (à adapter si nécessaire)
-df = pd.read_csv(csv_file)
+df = pd.read_csv(csv_file, sep=",")
+df_france = df[df["team"] == "France"]
+clubs = pd.unique(df_france["club"].dropna())
+print(df_france)
+print(len(df_france))
 
-# 4. Affiche les premières lignes
-print("\nPremières lignes du dataset :")
-print(df.head())
-
-# 5. Affiche les joueurs français
-print("\nJoueurs français :")
-print(df[df["team"] == "France"])
+nb_per_club=[]
+player_per_club=[]
+for i in clubs:
+    list_player = []
+    
+    nb=len(df_france[df_france["club"] == i])
+    name = df_france[df_france["club"] == i]["player"]
+    #print(df_france[df_france["club"] == i]["player"])
+    player_per_club.append(name)
+    print("nombre de joueur au club "+i+" : "+str(nb))
+    nb_per_club.append(nb)
+plt.bar(clubs, nb_per_club)
+print(player_per_club)
+plt.show()
